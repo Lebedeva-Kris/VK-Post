984e1da515ac105ee56a5b11ae65f4bdb89d8c9e
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-09 18:39:00 +0300

    PostAttachments class added

---
+++ src/main/java/domain/PostAttachments.java
@@ -0,0 +1,41 @@
+package domain;
+
+public class PostAttachments {
+    private int signerId;
+    private String photoUrl;
+    private String postType;
+    private boolean read;
+
+
+    public int getSignerId() {
+        return signerId;
+    }
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+
+    public String getPostType() {
+        return postType;
+    }
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+
+    public boolean isRead() {
+        return read;
+    }
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+}
