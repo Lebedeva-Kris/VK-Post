877d4e75d93612dc864e0751ad70f891578c8156
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-09 18:50:13 +0300

    PostLikes class renamed to LikesInfo

---
+++ src/main/java/domain/LikesInfo.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class LikesInfo {
+    private int likeId;
+    private int count;
+    private boolean like;
+
+
+    public int getLikeId() {
+        return likeId;
+    }
+    public void setLikeId(int likeId) {
+        this.likeId = likeId;
+    }
+
+
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+
+    public boolean isLike() {
+        return like;
+    }
+    public void setLike(boolean like) {
+        this.like = like;
+    }
+
+}
