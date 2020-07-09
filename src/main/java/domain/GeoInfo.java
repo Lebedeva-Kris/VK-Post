128bce45649644fdfbc0227658e04c39a28dd473
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-09 19:21:42 +0300

    GeoInfo class added

---
+++ src/main/java/domain/GeoInfo.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class GeoInfo {
+    private int geoId;
+    private String geoType;
+    private int Coordinates;
+
+
+    public int getGeoId() {
+        return geoId;
+    }
+    public void setGeoId(int geoId) {
+        this.geoId = geoId;
+    }
+
+
+    public String getGeoType() {
+        return geoType;
+    }
+    public void setGeoType(String geoType) {
+        this.geoType = geoType;
+    }
+
+
+    public int getCoordinates() {
+        return Coordinates;
+    }
+    public void setCoordinates(int coordinates) {
+        Coordinates = coordinates;
+    }
+
+}
