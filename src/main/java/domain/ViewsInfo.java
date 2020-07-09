b5f8892d0a4031abb310eb92c759b4031970172c
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-09 18:58:35 +0300

    ViewsInfo class added

---
+++ src/main/java/domain/ViewsInfo.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class ViewsInfo {
+    private int viewId;
+    private int viewCount;
+
+
+    public int getViewId() {
+        return viewId;
+    }
+    public void setViewId(int viewId) {
+        this.viewId = viewId;
+    }
+
+
+    public int getViewCount() {
+        return viewCount;
+    }
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
+
+}
