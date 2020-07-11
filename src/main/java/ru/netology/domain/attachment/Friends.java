9d0801cfaf9c1de8bba7fb5ddd371e9a31898766
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-11 21:17:54 +0300

    Friends array added

---
+++ src/main/java/ru/netology/domain/attachment/Friends.java
@@ -0,0 +1,32 @@
+package ru.netology.domain.attachment;
+
+public class Friends {
+    private int firstFriendId;
+    private int secondFriendId;
+    private int thirdFriendId;
+
+
+    public int getFirstFriendId() {
+        return firstFriendId;
+    }
+    public void setFirstFriendId(int firstFriendId) {
+        this.firstFriendId = firstFriendId;
+    }
+
+
+    public int getSecondFriendId() {
+        return secondFriendId;
+    }
+    public void setSecondFriendId(int secondFriendId) {
+        this.secondFriendId = secondFriendId;
+    }
+
+
+    public int getThirdFriendId() {
+        return thirdFriendId;
+    }
+    public void setThirdFriendId(int thirdFriendId) {
+        this.thirdFriendId = thirdFriendId;
+    }
+
+}
