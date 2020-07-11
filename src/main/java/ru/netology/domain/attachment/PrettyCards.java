c4a9c13ecb0bf59bf06021138f7933318c7c1be9
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-11 22:17:48 +0300

    PrettyCards attachment added

---
+++ src/main/java/ru/netology/domain/attachment/PrettyCards.java
@@ -0,0 +1,13 @@
+package ru.netology.domain.attachment;
+
+public class PrettyCards {
+    private Cards[] card;
+
+
+    public Cards[] getCard() {
+        return card;
+    }
+    public void setCard(Cards[] card) {
+        this.card = card;
+    }
+}
