3ae9e1d48bbb8986aaa3ab258c7afaba894b5e35
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-14 17:51:19 +0300

    Post added

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,50 +1,10 @@
 package ru.netology.domain;
 
 public class Post {
-    private int signerId;
-    private String photoUrl;
-    private String postType;
-    private boolean read;
-    private boolean favorite;
-
-
-    public boolean isFavorite() {
-        return favorite;
-    }
-    public void setFavorite(boolean favorite) {
-        this.favorite = favorite;
-    }
-
-
-    public int getSignerId() {
-        return signerId;
-    }
-    public void setSignerId(int signerId) {
-        this.signerId = signerId;
-    }
-
-
-    public String getPhotoUrl() {
-        return photoUrl;
-    }
-    public void setPhotoUrl(String photoUrl) {
-        this.photoUrl = photoUrl;
-    }
-
-
-    public String getPostType() {
-        return postType;
-    }
-    public void setPostType(String postType) {
-        this.postType = postType;
-    }
-
-
-    public boolean isRead() {
-        return read;
-    }
-    public void setRead(boolean read) {
-        this.read = read;
-    }
-
+    private int ownerId;
+    private String imageUrl;
+    private String groupName;
+    private String postAction;
+    private int date;
+    private boolean friendsOnly;
 }
